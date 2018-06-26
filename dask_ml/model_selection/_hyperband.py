76abea4a702092b6573d464e9f560cec7a573216
Scott Sievert <github@stsievert.com>
2018-06-26 17:57:08 -0500

    MAINT: move info to _hyperband.py and remove n_jobs

--- dask_ml/model_selection/_hyperband.py
+++ dask_ml/model_selection/_hyperband.py
@@ -10,11 +10,9 @@ from toolz import groupby
 from dask.distributed import as_completed, default_client, futures_of
 from dask_searchcv.model_selection import DaskBaseSearchCV
 import dask.array as da
-from ..datasets import make_classification
 from ..wrappers import Incremental
 from ._split import train_test_split
 from distributed.metrics import time
-from distributed import Future
 from copy import deepcopy
 
 
@@ -29,7 +27,7 @@ def _get_nr(R, eta=3):
 
 
 def _partial_fit(model_and_meta, X, y, meta=None, fit_params={},
-                 dry_run=False, **kwargs):
+                 **kwargs):
     start = time()
     assert isinstance(X, np.ndarray)
     assert y is None or isinstance(y, np.ndarray)
@@ -40,27 +38,19 @@ def _partial_fit(model_and_meta, X, y, meta=None, fit_params={},
         meta = deepcopy(meta)
     meta['mean_copy_time'] += time() - start
     while meta['iterations'] < meta['partial_fit_calls']:
-        if not dry_run:
-            model.partial_fit(X, y, **fit_params)
+        model.partial_fit(X, y, **fit_params)
         meta["iterations"] += 1
     meta['mean_fit_time'] += time() - start
     return model, meta
 
 
-def _score(model_and_meta, x, y, dry_run=False):
+def _score(model_and_meta, x, y):
     start = time()
     model = deepcopy(model_and_meta[0])
     meta = deepcopy(model_and_meta[1])
     meta['mean_copy_time'] += time() - start
-    if dry_run:
-        score = 0
-        #  BUG: uncommenting these lines gives a bug in test_info for R=27, 81
-        #  seed = hash(((k, v) for k, v in meta.items())) % 2**32
-        #  rng = np.random.RandomState(seed)
-        #  score = rng.rand()
-    else:
-        # TODO: change this to dask_ml.get_scorer
-        score = model.score(x, y)
+    # TODO: change this to dask_ml.get_scorer
+    score = model.score(x, y)
     meta.update(score=score)
     assert meta['iterations'] > 0
     meta['mean_score_time'] += time() - start
@@ -98,7 +88,7 @@ def _create_model(model, params, random_state=42):
     model = clone(model).set_params(**params)
     if 'random_state' in model.get_params():
         model.set_params(random_state=random_state)
-    #  meta = {}  # right now no meta information
+    # right now no meta information
     return model, None
 
 
@@ -111,27 +101,28 @@ def _model_id(s, n_i):
 
 
 async def _hyperband(model, params, X, y, max_iter=None, eta=None,
-                     dry_run=False, fit_params={}, random_state=42,
-                     asynchronous=None):
+                     fit_params={}, random_state=42, asynchronous=None):
     client = default_client()
     rng = check_random_state(random_state)
     N, R, brackets = _get_nr(max_iter, eta=eta)
-    params = iter(ParameterSampler(params, n_iter=sum(N),
-                                   random_state=random_state))
-    if isinstance(model, Incremental):  # this should probably be handled elsewhere
+    params = iter(ParameterSampler(params, n_iter=sum(N), random_state=rng))
+
+    # TODO: handle this unwrapping elsewhere
+    if isinstance(model, Incremental):
         model = model.estimator
+
     params = {_model_id(s, n_i): next(params)
               for s, n, r in zip(brackets, N, R) for n_i in range(n)}
     model_futures = {_model_id(s, n_i):
                      client.submit(_create_model, model,
                                    params[_model_id(s, n_i)],
-                                   random_state=random_state)
+                                   random_state=rng)
                      for s, n, r in zip(brackets, N, R) for n_i in range(n)}
 
     # lets assume everything in fit_params is small and make it concrete
     fit_params = await client.compute(fit_params)
 
-    r = train_test_split(X, y, test_size=0.15, random_state=random_state)
+    r = train_test_split(X, y, test_size=0.15, random_state=rng)
     X_train, X_test, y_train, y_test = r
     if isinstance(X, da.Array):
         X_train = futures_of(X_train.persist())
@@ -165,21 +156,19 @@ async def _hyperband(model, params, X, y, max_iter=None, eta=None,
                                         X_train[idx[(i, j)]],
                                         y_train[idx[(i, j)]],
                                         meta=meta, id=meta['model_id'],
-                                        dry_run=dry_run,
-                                        fit_params=fit_params, pure=False)
+                                        fit_params=fit_params, pure=True)
                           for i, bracket_metas in enumerate(info.values())
                           for j, meta in enumerate(bracket_metas)}
 
     assert set(model_meta_futures.keys()) == set(model_futures.keys())
 
     score_futures = [client.submit(_score, model_meta_future, X_test, y_test,
-                                   dry_run=dry_run, pure=False)
+                                   pure=True)
                      for _id, model_meta_future in model_meta_futures.items()]
 
     completed_jobs = {}
     seq = as_completed(score_futures)
-    all_models = {k: [v]
-                        for k, v in deepcopy(model_meta_futures).items()}
+    all_models = {k: [v] for k, v in deepcopy(model_meta_futures).items()}
     history = []
     async for future in seq:
         result = await future
@@ -189,7 +178,6 @@ async def _hyperband(model, params, X, y, max_iter=None, eta=None,
         jobs = _to_promote(result, completed_jobs.values(), eta=eta,
                            asynchronous=asynchronous)
         for job in jobs:
-            assert job['iterations'] > 0
             i = rng.choice(len(X_train))
 
             # This block prevents communication of the model
@@ -197,14 +185,13 @@ async def _hyperband(model, params, X, y, max_iter=None, eta=None,
             model_meta_future = model_meta_futures[model_id]
             model_meta_future = client.submit(_partial_fit, model_meta_future,
                                               X_train[i], y_train[i],
-                                              meta=job, dry_run=dry_run,
-                                              id=model_id,
+                                              meta=job, id=model_id,
                                               fit_params=fit_params)
             all_models[model_id] += [model_meta_future]
             model_meta_futures[model_id] = model_meta_future
 
             score_future = client.submit(_score, model_meta_future, X_test,
-                                         y_test, dry_run=dry_run)
+                                         y_test)
             seq.add(score_future)
 
     assert completed_jobs.keys() == model_meta_futures.keys()
@@ -241,14 +228,14 @@ class HyperbandCV(DaskBaseSearchCV):
                              asynchronous=self.asynchronous,
                              fit_params=fit_params, eta=self.eta,
                              random_state=self.random_state)
-        params, model_meta_futures, history, all_models, jobs = r
+        params, model_meta_futures, history, all_models, meta = r
 
-        self.jobs_ = jobs
+        self.meta_ = meta
         self.history_ = history
         self._model_meta_futures = model_meta_futures
         self.__all_models = all_models
 
-        cv_res, best_idx = _get_cv_results(jobs, params)
+        cv_res, best_idx = _get_cv_results(meta, params)
         best_id = cv_res['model_id'][best_idx]
 
         best_model_and_meta = yield model_meta_futures[best_id]
@@ -271,39 +258,28 @@ class HyperbandCV(DaskBaseSearchCV):
         models = {k: [vi[0] for vi in v] for k, v in models_and_meta.items()}
         return models
 
-    def info(self, jobs=None):
-        res = default_client().sync(self._info, jobs=jobs)
-        assert isinstance(res, dict)
-        return res
+    def info(self, meta=None):
+        if meta is None:
+            bracket_info = _hyperband_paper_alg(self.max_iter, eta=self.eta)
+            total_models = sum(b['num_models'] for b in bracket_info)
+        else:
+            brackets = groupby("bracket", meta)
+            bracket_info = [{'num_models': max(vi['num_models'] for vi in v),
+                             'partial_fit_calls': sum(vi['partial_fit_calls']
+                                                      for vi in v),
+                             'bracket': k,
+                             'iters': {vi['partial_fit_calls'] for vi in v}}
+                            for k, v in brackets.items()]
+            total_models = len(set([r['model_id'] for r in meta]))
+        for bracket in bracket_info:
+            bracket['iters'] = sorted(list(bracket['iters']))
+        total_partial_fit = sum(b['partial_fit_calls'] for b in bracket_info)
+        bracket_info = sorted(bracket_info, key=lambda x: x['bracket'])
 
-    @gen.coroutine
-    def _info(self, jobs=None):
-        if jobs is None:
-            X, y = make_classification(n_samples=10, n_features=4, chunks=10,
-                                       random_state=self.random_state)
-            r = yield _hyperband(self.model, self.params, X, y,
-                                 max_iter=self.max_iter,
-                                 asynchronous=self.asynchronous,
-                                 dry_run=True, eta=self.eta,
-                                 random_state=self.random_state)
-            _, _, _, _, jobs = r
-
-        brackets = groupby("bracket", jobs)
-        keys = sorted(brackets.keys())
-        values = [brackets[k] for k in keys]
-        bracket_info = [{'num_models': max(r['num_models'] for r in v),
-                         'partial_fit_calls': sum(r['partial_fit_calls']
-                                                  for r in v),
-                         'bracket': k}
-                        for k, v in zip(keys, values)]
-        info = {'total_partial_fit_calls': sum(r['partial_fit_calls']
-                                               for r in jobs),
-                'total_models': len(set([r['model_id']
-                                         for r in jobs]))}
-        res = {'brackets': bracket_info}
-        res.update(**info)
-        assert isinstance(res, dict)
-        return res
+        info = {'total_partial_fit_calls': total_partial_fit,
+                'total_models': total_models,
+                'brackets': bracket_info}
+        return info
 
 
 def _get_cv_results(history, params):
@@ -323,3 +299,47 @@ def _get_cv_results(history, params):
     cv_results['rank_test_score'] = ranks.tolist()
     cv_results.update(params)
     return cv_results, best_idx
+
+
+def _hyperband_paper_alg(R, eta=3):
+    """
+    Algorithm 1 from the Hyperband paper. Only a slight modification is made,
+    the ``if to_keep <= 1``: if 1 model is left there's no sense in training
+    any further.
+
+    References
+    ----------
+    1. "Hyperband: A novel bandit-based approach to hyperparameter
+       optimization", 2016 by L. Li, K. Jamieson, G. DeSalvo, A. Rostamizadeh,
+       and A. Talwalkar.  https://arxiv.org/abs/1603.06560
+    """
+    s_max = math.floor(math.log(R, eta))
+    B = (s_max + 1) * R
+    brackets = reversed(range(int(s_max + 1)))
+    hists = {}
+    for s in brackets:
+        n = int(math.ceil(B / R * eta**s / (s + 1)))
+        r = int(R * eta**-s)
+
+        T = set(range(n))
+        hist = {'num_models': n, 'models': {n: 0 for n in range(n)},
+                'iters': []}
+        for i in range(s + 1):
+            n_i = math.floor(n * eta**-i)
+            r_i = r * eta**i
+            L = {model: r_i for model in T}
+            hist['models'].update(L)
+            hist['iters'] += [r_i]
+            to_keep = math.floor(n_i / eta)
+            T = {model for i, model in enumerate(T)
+                 if i < to_keep}
+            if to_keep <= 1:
+                break
+
+        hists['bracket={s}'.format(s=s)] = hist
+
+    info = [{'bracket': k, 'num_models': hist['num_models'],
+             'partial_fit_calls': sum(hist['models'].values()),
+             'iters': set(hist['iters'])}
+            for k, hist in hists.items()]
+    return info
