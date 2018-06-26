b803c50ea8398acbd61fe79fb02c1b4198725434
Scott Sievert <github@stsievert.com>
2018-06-26 17:57:08 -0500

    MAINT: move info to _hyperband.py and remove n_jobs

--- tests/model_selection/test_hyperband.py
+++ tests/model_selection/test_hyperband.py
@@ -44,7 +44,8 @@ def test_sklearn(array_type, library, loop, max_iter=27):
 
             kwargs = dict(tol=1e-3, penalty='elasticnet', random_state=42)
             models = {'sklearn': SGDClassifier(**kwargs),
-                      'dask-ml': Incremental(SGDClassifier(), **kwargs),
+                      'dask-ml': Incremental(SGDClassifier(random_state=42),
+                                             **kwargs),
                       'test': ConstantFunction()}
             sgd_params = {'alpha': np.logspace(-2, 1, num=1000),
                           'l1_ratio': np.linspace(0, 1, num=1000),
@@ -68,12 +69,12 @@ def test_sklearn(array_type, library, loop, max_iter=27):
                 # Test fraction of 0.15 is hardcoded into _hyperband
                 return (t_ - 1) / (chunk_size * (1 - 0.15))
             iters = {_iters(model) for model in models.values()}
+            assert 1 <= min(iters) < max(iters) <= max_iter
 
             info_plain = search.info()
-            info_train = search.info(jobs=search.jobs_)
+            info_train = search.info(meta=search.meta_)
             assert info_plain['brackets'] == info_train['brackets']
             assert info_train == info_plain
-            assert 1 <= min(iters) < max(iters) <= max_iter
 
 
 @gen_cluster(client=True)
@@ -113,33 +114,21 @@ async def test_sklearn_async(c, s, a, b):
 def test_info(loop, max_iter):
     with cluster() as (s, [a, b]):
         with Client(s['address'], loop=loop) as c:
+            X, y = make_classification(chunks=5, n_features=5)
             model = ConstantFunction()
             params = {'value': stats.uniform(0, 1)}
             alg = HyperbandCV(model, params, max_iter=max_iter, random_state=0,
                               asynchronous=False)
-            info = alg.info()
-            paper_alg_info = _hyperband_paper_alg(max_iter)
-            saved = {81: {'bracket=4': 243, 'bracket=3': 222, 'bracket=2': 225,
-                          'bracket=1': 324, 'bracket=0': 405},
-                     27: {'bracket=3': 63, 'bracket=2': 60, 'bracket=1': 90,
-                          'bracket=0': 108},
-                     9: {'bracket=2': 15, 'bracket=1': 15, 'bracket=0': 27},
-                     3: {'bracket=1': 3, 'bracket=0': 6}}
-            print(info)
-            assert paper_alg_info == saved[max_iter]
-            assert (info['total_partial_fit_calls'] ==
-                    sum(paper_alg_info.values()) )#==
-                    #  sum(b['partial_fit_calls'] for b in info['brackets']))
-            for bracket in info['brackets']:
-                k = bracket['bracket']
-                assert bracket['partial_fit_calls'] == paper_alg_info[k]
-            assert (info['total_models'] ==
-                    sum(b['num_models'] for b in info['brackets']))
-
-            X, y = make_classification(n_samples=10, n_features=4, chunks=10)
             alg.fit(X, y)
-            info_after_fit = alg.info(jobs=alg.jobs_)
-            assert info_after_fit == info
+            paper_info = alg.info()
+            actual_info = alg.info(meta=alg.meta_)
+            assert paper_info['total_models'] == actual_info['total_models']
+            assert (paper_info['total_partial_fit_calls'] ==
+                    actual_info['total_partial_fit_calls'])
+            from pprint import pprint
+            pprint(paper_info['brackets'])
+            pprint(actual_info['brackets'])
+            assert paper_info['brackets'] == actual_info['brackets']
 
 
 def test_integration(loop):
@@ -206,42 +195,3 @@ def test_copy(loop):
     for models in copied_models:
         assert all(not np.allclose(m1.coef_, m2.coef_)
                    for m1, m2 in itertools.combinations(models, 2))
-
-
-def _hyperband_paper_alg(R, eta=3):
-    """
-    Algorithm 1 from the Hyperband paper. Only a slight modification is made,
-    the ``if to_keep <= 1``: if 1 model is left there's no sense in training
-    any further.
-
-    References
-    ----------
-    1. "Hyperband: A novel bandit-based approach to hyperparameter optimization",
-       2016 by L. Li, K. Jamieson, G. DeSalvo, A. Rostamizadeh, and A. Talwalkar.
-       https://arxiv.org/abs/1603.06560
-    """
-    s_max = math.floor(math.log(R, eta))
-    B = (s_max + 1) * R
-    brackets = reversed(range(int(s_max + 1)))
-    hists = {}
-    for s in brackets:
-        n = int(math.ceil(B / R * eta**s / (s + 1)))
-        r = int(R * eta**-s)
-
-        T = {n for n in range(n)}
-        hist = {n: 0 for n in range(n)}
-        for i in range(s + 1):
-            n_i = math.floor(n * eta**-i)
-            r_i = r * eta**i
-            L = {model: r_i for model in T}
-            hist.update(L)
-            to_keep = math.floor(n_i / eta)
-            T = {model for i, model in enumerate(T)
-                 if i < to_keep}
-            if to_keep <= 1:
-                break
-
-        hists['bracket={s}'.format(s=s)] = hist
-
-    num_partial_fit_calls = {k: sum(v.values()) for k, v in hists.items()}
-    return num_partial_fit_calls
