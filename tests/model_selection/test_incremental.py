32f8ce837af9d6e4b213d930b1333134eb8c4bd0
Scott Sievert <github@stsievert.com>
2018-08-24 13:46:54 -0500

    TST: adapt tests to API changes and get (mostly) working

--- tests/model_selection/test_incremental.py
+++ tests/model_selection/test_incremental.py
@@ -7,6 +7,9 @@ from tornado import gen
 
 from dask_ml.datasets import make_classification
 from dask_ml.model_selection._incremental import fit, _partial_fit
+from sklearn.model_selection import ParameterSampler
+import distributed
+import random
 
 
 @gen_cluster(client=True, timeout=None)
@@ -22,31 +25,44 @@ def test_basic(c, s, a, b):
     X_train = X[1000:]
     y_train = y[1000:]
 
-    info, model, history = yield fit(model, params,
-                                     X_train, y_train,
-                                     X_test, y_test,
-                                     start=100,
-                                     fit_params={'classes': [0, 1]})
+    param_list = list(ParameterSampler(params, 100))
+    def update(info):
+        pf_calls = {k: v[-1]['partial_fit_calls'] for k, v in info.items()}
+        ret = {k: int(calls < 10) for k, calls in pf_calls.items()}
+        # Don't train one model
+        del ret[random.choice(list(ret.keys()))]
+        return ret
+    info, models, history = yield fit(model, param_list,
+                                      X_train, y_train,
+                                      X_test, y_test,
+                                      additional_partial_fit_calls=update,
+                                      fit_params={'classes': [0, 1]})
 
     # Ensure that we touched all data
     keys = {t[0] for t in s.transition_log}
     L = [str(k) in keys for kk in X_train.__dask_keys__() for k in kk]
     assert all(L)
 
-    assert isinstance(model, SGDClassifier)
+    for model in models.values():
+        assert isinstance(model, distributed.client.Future)
+        model2, meta2 = yield model
+        assert isinstance(model2, SGDClassifier)
+        assert isinstance(meta2, dict)
 
     while c.futures or s.tasks:  # Cleans up cleanly after running
         yield gen.sleep(0.01)
 
-    # XX_test, yy_test = yield c.compute([X_test, y_test])
-    # assert model.score(XX_test, yy_test) == info['score']
+    XX_test, yy_test = yield c.compute([X_test, y_test])
+    model, meta = yield models[0]
+    assert model.score(XX_test, yy_test) == info[0][-1]['score']
 
-    assert len(history) > 200
+    # `<` not `==` because we randomly dropped one model
+    assert len(history) < 100 * 10
 
     groups = toolz.groupby('partial_fit_calls', history)
-    assert (len(groups[0]) > len(groups[1]) >
-            len(groups[2]) > len(groups[max(groups)]))
-    assert max(groups) > 10
+    assert (len(groups[1]) > len(groups[2]) >
+            len(groups[3]) > len(groups[max(groups)]))
+    assert max(groups) == 10
 
 
 def test_partial_fit_doesnt_mutate_inputs():
@@ -69,7 +85,7 @@ def test_partial_fit_doesnt_mutate_inputs():
         fit_params={"classes": np.unique(y)}
     )
     assert meta != new_meta
-    assert new_meta["iterations"] == 1
+    assert new_meta["partial_fit_calls"] == 2
     assert not np.allclose(model.coef_, new_model.coef_)
     assert model.t_ < new_model.t_
 
@@ -83,6 +99,7 @@ def test_explicit(c, s, a, b):
     def update(scores):
         """ Progress through predefined updates, checking along the way """
         ts = scores[0][-1]['partial_fit_calls']
+        ts -= 1  # partial_fit_calls = time step + 1
         if ts == 0:
             assert len(scores) == len(params)
             assert len(scores[0]) == 1
@@ -107,7 +124,7 @@ def test_explicit(c, s, a, b):
 
     info, models, history = yield fit(model, params, X, y,
                                       X.blocks[-1], y.blocks[-1],
-                                      update=update, scorer=None,
+                                      additional_partial_fit_calls=update, scorer=None,
                                       fit_params={'classes': [0, 1]})
     assert all(model.done() for model in models.values())
 
@@ -115,7 +132,7 @@ def test_explicit(c, s, a, b):
     model, meta = models[0]
 
     assert meta['params'] == {'alpha': 0.1}
-    assert meta['partial_fit_calls'] == 6
+    assert meta['partial_fit_calls'] == 6 + 1
     assert len(models) == len(info) == 1
     assert meta['partial_fit_calls'] == history[-1]['partial_fit_calls']
 
