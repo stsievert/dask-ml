d7a83530bba3d565b5e5322ea4157c53f7570ecb
Scott Sievert <dev@stsievert.com>
2018-11-11 21:25:42 -0600

    ENH: add black box for model selection
    This allows fitting a (random) subset of the data on any partial_fit
    calls. This class fulfills the 'dataset subsampling' use case mentioned
    on page 10 of the Hyperband of the Hyperband paper
    (https://arxiv.org/pdf/1603.06560.pdf).

--- dask_ml/model_selection/__init__.py
+++ dask_ml/model_selection/__init__.py
@@ -5,9 +5,11 @@ on the underlying estimators being used.
 """
 from ._search import GridSearchCV, RandomizedSearchCV, compute_n_splits, check_cv
 from ._split import ShuffleSplit, KFold, train_test_split
+from ._black_box import BlackBox
 
 
 __all__ = [
+    "BlackBox",
     "GridSearchCV",
     "RandomizedSearchCV",
     "ShuffleSplit",
